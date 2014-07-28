c7f1e1a8f92365f9b49b1150031506db3e6bbca8
hisaichi5518 <hisaichi5518@gmail.com>
2014-07-28 11:12:53 +0900

    設定の書き換え

--- config/common.sample.pl
+++ config/common.sample.pl
@@ -1,12 +1,10 @@
 {
     'DBIx::Handler' => [
         'dbi:Pg:host=<host>;port=<port>;database=<database>',
-        'user',
-        'password',
+        '<user>',
+        '<password>',
     ],
     boostrap => {
         web => ['-p' => 50004],
     },
-    #num_keys => [qw/uid/],
-    #str_keys => [qw/type/],
 }
